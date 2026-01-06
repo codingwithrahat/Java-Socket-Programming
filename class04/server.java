9f0d9be72c1c09992dbe23834d415341dff1e341
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2026-01-07 00:20:25 +0600

    Update server.java

--- class04/server.java
+++ class04/server.java
@@ -17,56 +17,64 @@ public class server {
             System.out.println("Connected");
 
             BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-            String str = in.readLine();
-
-            char[] s = str.toCharArray();
-
-            boolean flag = false;
-
-            if(s.length >= 5 && s[0] == '[' && s[s.length - 1] == ']'){
-                int colidx = -1;
-
-                for(int i = 1; i< s.length - 1; i++){
-                    if(s[i] == ':'){
-                        colidx = i;
-                        break;
-                    }
-                }
-
-                if(colidx != -1){
-                    String num = "";
-
-                    for(int i = 1; i<colidx; i++) {
-                        num += s[i];
-                    }
-
-                    int len = 0;
-
-                    for(int i = 0; i<num.length(); i++){
-                        len = (len * 10) + (num.charAt(i) - '0');
-                    }
-
-                    int cnt = 0;
-
-                    for(int i = colidx + 1; i<s.length - 1; i++){
-                        cnt++;
-                    }
-
-                    if(len == cnt){
-                        flag = true;
-                    }
-
-                }
-
-            }
-
             PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
 
-            if(flag){
-                out.println("Mssg Accepted : " + str);
-            }else{
-                out.println("Invalid Format : " + str);
+            String str;
+
+            while(true){
+
+                str = in.readLine();
+
+                if(str.equals("bye")) break;
+
+                char[] s = str.toCharArray();
+
+                boolean flag = false;
+
+                if(s.length >= 5 && s[0] == '[' && s[s.length - 1] == ']'){
+                    int colidx = -1;
+
+                    for(int i = 1; i< s.length - 1; i++){
+                        if(s[i] == ':'){
+                            colidx = i;
+                            break;
+                        }
+                    }
+
+                    if(colidx != -1){
+                        String num = "";
+
+                        for(int i = 1; i<colidx; i++) {
+                            num += s[i];
+                        }
+
+                        int len = 0;
+
+                        for(int i = 0; i<num.length(); i++){
+                            len = (len * 10) + (num.charAt(i) - '0');
+                        }
+
+                        int cnt = 0;
+
+                        for(int i = colidx + 1; i<s.length - 1; i++){
+                            cnt++;
+                        }
+
+                        if(len == cnt){
+                            flag = true;
+                        }
+
+                    }
+
+                }
+
+                if(flag){
+                    out.println("Mssg Accepted : " + str);
+                }else{
+                    out.println("Invalid Format : " + str);
+                }
             }
+
 
         } catch (Exception e){
             System.out.println(e);
