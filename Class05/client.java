a6dbe6f791e3b6bda4d7c8e4823104267e9264ce
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2026-01-22 01:12:16 +0600

    Update client.java

--- Class05/client.java
+++ Class05/client.java
@@ -1,64 +1,133 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Random;
 
-public class client {
 
-    public static String format(String s){
-        return ("[" + s.length() + ":" + s + "]");
+public class server {
+
+
+    static int cntCorrect = 0;                             //use static cz these are global variable
+    static int cntWrong = 0;
+
+    public static double rate(){                          //success rate function
+        int sum = cntCorrect + cntWrong;
+
+        if(sum == 0) return 0.0;                          // avoid division by zero
+
+        return ((cntCorrect * 100.0) / sum);
     }
 
-    public static int generateNum(){
-        Random rNum = new Random();
-        int num = rNum.nextInt();          //generate a random number, number can be negative also
-        num = Math.abs(num);               //abs function will make all num positive
-        num = num % 100;                   // it make sure the number is between 0 to 99
 
-        return num;
-
-    }
 
     public static void main(String[] args){
 
         try{
 
-            System.out.println("Client Started");
-            Socket soc = new Socket("localhost", 5000);
+            System.out.println("Waiting for client");
 
-            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            ServerSocket ss = new ServerSocket(5000);
+            Socket soc = ss.accept();
 
-            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+            System.out.println("Connected");
 
             BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
 
-            String str;
+            String str, strNum;
 
             while(true){
 
-                System.out.print("Enter Mssg : ");
-                str = userInput.readLine();
+                str = in.readLine();
 
-                out.println(format(str));
+                if (str == null || str.equalsIgnoreCase("bye")) {
+                    break;
+                }
 
-                out.println(generateNum());                 //send random number to server
+                strNum = in.readLine();                  //socket can only read string
+
+                int rNum = Integer.parseInt(strNum);     //convert string to int
 
 
-                if (str.equalsIgnoreCase("bye")) break;
+                if(rNum <= 80){
 
-                String str2 = in.readLine();
-                System.out.println(str2);
+                    // Show the percentage of correct & error
+                    System.out.println("Correct : " + (rNum) + "%" + " || " + "Error : " + (100 - rNum) + "%");
 
+                    cntCorrect++;
+
+                    char[] s = str.toCharArray();
+
+                    boolean flag = false;
+
+                    if(s.length >= 5 && s[0] == '[' && s[s.length - 1] == ']'){
+                        int colidx = -1;
+
+                        for(int i = 1; i< s.length - 1; i++){
+                            if(s[i] == ':'){
+                                colidx = i;
+                                break;
+                            }
+                        }
+
+                        if(colidx != -1){
+                            String num = "";
+
+                            for(int i = 1; i<colidx; i++) {
+                                num += s[i];
+                            }
+
+                            int len = 0;
+
+                            for(int i = 0; i<num.length(); i++){
+                                len = (len * 10) + (num.charAt(i) - '0');
+                            }
+
+                            int cnt = 0;
+
+                            for(int i = colidx + 1; i<s.length - 1; i++){
+                                cnt++;
+                            }
+
+                            if(len == cnt){
+                                flag = true;
+                            }
+
+                        }
+
+                    }
+
+                    if(flag){
+                        System.out.println("Client Syas : " + str);
+                        out.println("Mssg Accepted : " + str);
+                    }else{
+                        out.println("Error");
+                        System.out.println("Invalid Format");
+                    }
+
+
+                }else{
+
+                    // Show the percentage of correct & error
+                    System.out.println("Correct : " + (rNum) + "%" + " " + "Error : " + (100 - rNum) + "%");
+                    cntWrong++;
+                    out.println("Error");
+                }
+
+
+                double sucRate = rate();
+                System.out.println("Sucess rate: " + sucRate);
 
             }
 
-            System.out.println("Connection Lost.");
+            System.out.println("Connection Close");
 
+            ss.close();
             soc.close();
 
-        } catch (Exception e) {
+
+        } catch (Exception e){
             System.out.println(e);
         }
 
